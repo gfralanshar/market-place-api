6be83971c979cb61a36f069555476407c657518b
Gifar Al Anshar <dailycoding62@gmail.com>
2024-01-31 17:13:45 +0700

    Add create user action

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -12,4 +12,20 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(response.body)
     assert_equal @user.email, json_response['email']
   end
+
+  test 'should create a new user if valid user' do
+    assert_difference "User.count", 1 do
+      post api_v1_users_path, params: {user: {email: "foo@gmail.com", password: "secret"}}, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test 'should not create a new user if invalid user' do
+    assert_no_difference "User.count" do
+      post api_v1_users_path, params: { user: {email: @user.email, password: "secret"}}, as: :json
+    end
+
+    assert_response :unprocessable_entity
+  end
 end
